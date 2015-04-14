4c79c921531d4f1f3fac412b6c2bba97391f8314
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2015-04-14 17:03:22 +0800

    added angularjs

--- fbt-ui2/static/js/app.js
+++ fbt-ui2/static/js/app.js
@@ -1,16 +1,28 @@
+/* vim: set sw=2 ts=2 : */
 'use strict';
 
-var app = angular.module('uiApp', []);
+var app = angular.module('fbtApp', []);
 app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/home',
-            {
-                templateUrl: '/static/partials/home.html'
-            })
-        .when('/photos',
-            {
-                controller: 'PhotoController',
-                templateUrl: '/static/partials/photos.html'
-            })
-        .otherwise({ redirectTo: '/home' });
+  $routeProvider
+    .when('/home',
+        {
+            controller: 'HomeController',
+            templateUrl: 'static/partials/home.html'
+        })
+    .when('/resource',
+        {
+            controller: 'ResourceController',
+            templateUrl: 'static/partials/resource.html'
+        })
+    .when('/circle',
+        {
+            controller: 'CircleController',
+            templateUrl: 'static/partials/circle.html'
+        })
+    .when('/reward',
+        {
+            controller: 'RewardController',
+            templateUrl: 'static/partials/reward.html'
+        })
+    .otherwise({ redirectTo: '/home' });
 });
--- fbt-ui2/static/js/controllers.js
+++ fbt-ui2/static/js/controllers.js
@@ -1,26 +1,13 @@
 'use strict';
 
-app.controller('PhotoController', function($scope, Photo) {
-    Photo.query(function(data) {
-        $scope.rows = [];
-        var i = 0;
-        for(; i<data.length/3-1|0; i++) {
-            var row = [];
-            row.push(data[i*3]);
-            row.push(data[i*3+1]);
-            row.push(data[i*3+2]);
-            $scope.rows.push(row);
-        }
-        var row = [];
-        for(var j=i*3; j<data.length; j++) {
-            row.push(data[j]);
-        }
-        $scope.rows.push(row);
-    });
+app.controller('HomeController', function($scope) {
 });
 
-app.controller('NavbarController', function($scope, $location) {
-    $scope.getActive = function(path) {
-        return $location.url().split('?')[0] == path;
-    }
+app.controller('ResourceController', function($scope) {
+});
+
+app.controller('CircleController', function($scope) {
+});
+
+app.controller('RewardController', function($scope) {
 });
